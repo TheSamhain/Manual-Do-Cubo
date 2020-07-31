07a381be3fb32099a866f721e9e32c367f5154e5
Leonardo Noro <leon.np99@gmail.com>
2020-07-31 20:02:18 -0300

    Alterado tema do app

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,7 +7,7 @@ function Menu() {
   return (
     <nav className="Menu">
       <a href="/">
-        <img className="Logo" src={Logo} alt="AliceFlix Logo" border="0" />
+        <img className="Logo" src={Logo} alt="MaratonaOtaku Logo" border="0" />
       </a>
 
       <Button as="a" className="ButtonLink" href="/" >
