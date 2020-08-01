14f2e34f9987dfd964e2d6524467ab58db8d3a48
Leonardo Noro <leon.np99@gmail.com>
2020-07-31 23:01:33 -0300

    Aula 2 #ImersaoReact

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -6,11 +8,11 @@ import Button from '../Button';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="MaratonaOtaku Logo" border="0" />
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/" >
+      <Button as={Link} className="ButtonLink" to="/cadastro/video" >
         Novo Video
       </Button>
     </nav>
